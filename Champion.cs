6b0b4b797e5a84e328c94072857aa70f93680fec
mariothedog <mariothedogmc@gmail.com>
2019-05-28 22:14:56 +0100

    Update Champion.cs

--- Champion.cs
+++ Champion.cs
@@ -3,31 +3,12 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.Localization;
 
-namespace SingleSwordMod
+namespace Champion
 {
-	class SingleSwordMod : Mod
+	class Champion : Mod
 	{
-		public SingleSwordMod()
+		public Champion()
 		{
-        }
-
-        public override void AddRecipeGroups()
-        {
-            RecipeGroup group = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Common Broadswords", new int[]
-            {
-                    ItemID.LeadBroadsword,
-                    ItemID.IronBroadsword
-            });
-            RecipeGroup.RegisterGroup("SingleSwordMod:CommonBroadswords", group);
-        }
-
-        public override void AddRecipes()
-        {
-            ModRecipe recipe = new ModRecipe(this);
-            recipe.AddIngredient(ItemID.SlimeStaff);
-            recipe.AddRecipeGroup("SingleSwordMod:CommonBroadswords");
-            recipe.SetResult(ItemID.IronHammer);
-            recipe.AddRecipe();
-        }
-    }
+		}
+	}
 }
