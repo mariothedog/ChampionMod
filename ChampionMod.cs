ac780fd710170d0432b9c7925f2f1b28410f5918
mariothedog <mariothedogmc@gmail.com>
2019-06-02 22:24:14 +0100

    Renamed Champion.cs to ChampionMod.cs

---
+++ ChampionMod.cs
@@ -0,0 +1,14 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Localization;
+
+namespace Champion
+{
+	class Champion : Mod
+	{
+		public Champion()
+		{
+		}
+	}
+}
