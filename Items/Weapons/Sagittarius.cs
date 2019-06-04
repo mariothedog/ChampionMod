cec3f3d4bebe14afb6fb5a0d9f4ce73d8a58b274
mariothedog <mariothedogmc@gmail.com>
2019-06-04 18:23:58 +0100

    Sagittarius

---
+++ Items/Weapons/Sagittarius.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ChampionMod.Items.Weapons
+{
+    public class Sagittarius : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.damage = 24;
+            item.ranged = true;
+            item.width = 40;
+            item.height = 20;
+            item.useTime = 23;
+            item.useAnimation = 23;
+            item.useStyle = 5;
+            item.noMelee = true;
+            item.knockBack = 4;
+            item.value = 10000;
+            item.rare = 2;
+            item.UseSound = SoundID.Item5;
+            item.autoReuse = true;
+            item.shoot = 10;
+            item.shootSpeed = 10f;
+            item.useAmmo = AmmoID.Arrow;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.PlatinumBow, 1);
+            recipe.AddIngredient(ItemID.FallenStar, 5);
+            recipe.AddIngredient(ItemID.PixieDust, 5);
+            recipe.AddIngredient(ItemID.SoulofFlight, 10);
+            recipe.AddIngredient(ItemID.HallowedBar, 8);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            float numberProjectiles = 3; 
+            float rotation = MathHelper.ToRadians(5); // So the jester's arrows don't fly in the same position (adds spread)
+            
+            position += Vector2.Normalize(new Vector2(speedX, speedY));
+            if (type == ProjectileID.WoodenArrowFriendly) 
+            {
+                type = ProjectileID.JestersArrow;
+            }
+
+            for (int i = 0; i < numberProjectiles; i++)
+            {
+                Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedBy(MathHelper.Lerp(-rotation, rotation, i / (numberProjectiles - 1)));
+                Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI);
+            }
+        
+            return false; // False so it doesn't create another projectile like normal
+        }
+    }
+}
