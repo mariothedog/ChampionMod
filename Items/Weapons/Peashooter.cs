7f39e1f1b0aa7804121cc55a3477527d5abca77a
mariothedog <mariothedogmc@gmail.com>
2019-06-05 21:51:51 +0100

    Peashooter

---
+++ Items/Weapons/Peashooter.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria; 
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ChampionMod.Items.Weapons
+{
+    public class Peashooter : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Not to be confused with a certain plant.");
+        }
+
+        public override void SetDefaults() {
+            item.damage = 3;
+            item.ranged = true;
+            item.width = 36;
+            item.height = 22;
+            item.useTime = 23;
+            item.useAnimation = 23;
+            item.useStyle = 5;
+            item.noMelee = true;
+            item.knockBack = 2;
+            item.UseSound = SoundID.Item11;
+            item.shoot = 10;
+            item.shootSpeed = 7f;
+            item.useAmmo = AmmoID.Bullet;
+
+            // Value and rarity are both 0 (that is the default)
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Wood, 10);
+            recipe.AddIngredient(ItemID.StoneBlock, 5);
+            recipe.AddTile(TileID.Anvils);
+        }
+
+        // So bullet comes out of muzzle
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+            position.Y -= 9;
+			return true;
+		}
+    }
+}
