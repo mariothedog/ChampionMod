861147dc49e9aaf0dfd3a33b1c5e754fdfb76a7c
mariothedog <mariothedogmc@gmail.com>
2019-06-06 21:34:05 +0100

    Ichor Blaster updates

--- Items/Weapons/IchorBlaster.cs
+++ Items/Weapons/IchorBlaster.cs
@@ -15,6 +15,7 @@ namespace ChampionMod.Items.Weapons
         {
             item.damage = 18;
             item.ranged = true;
+            item.noMelee = true;
             item.width = 40;
             item.height = 40;
             item.useTime = 22;
@@ -23,7 +24,7 @@ namespace ChampionMod.Items.Weapons
             item.knockBack = 3;
             item.value = 600000;
             item.rare = 4;
-            item.UseSound = SoundID.Item12;
+            item.UseSound = SoundID.Item11;
             item.autoReuse = true;
             item.shoot = 279;
             item.shootSpeed = 13f;
@@ -33,12 +34,21 @@ namespace ChampionMod.Items.Weapons
 
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
         {
+            // Change bullet to an ichor bullet
             if (type == ProjectileID.Bullet)
             {
                 type = ProjectileID.IchorBullet;
             }
 
+            position.Y -= 5;
+
             return true; // return true to allow tmodloader to call Projectile.NewProjectile as normal
         }
+
+        public override Vector2? HoldoutOffset()
+		{
+			return new Vector2(0, 0);
+		}
+        
     }
 }
