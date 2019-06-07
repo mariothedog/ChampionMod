f53f16a54fe068ee67d1c2f467f8a5e914c41980
mariothedog <mariothedogmc@gmail.com>
2019-06-07 23:55:37 +0100

    Update ChampionMod.cs

--- ChampionMod.cs
+++ ChampionMod.cs
@@ -12,20 +12,20 @@ namespace ChampionMod
 		}
 
 		public override void AddRecipeGroups()
-        {
-            RecipeGroup group = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Tier 2 Broadswords", new int[]
-            {
-                    ItemID.LeadBroadsword,
-                    ItemID.IronBroadsword
-            });
-            RecipeGroup.RegisterGroup("ChampionMod:Tier2Broadswords", group);
+                        {
+                        RecipeGroup group = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Tier 2 Broadswords", new int[]
+                        {
+                                ItemID.LeadBroadsword,
+                                ItemID.IronBroadsword
+                        });
+                        RecipeGroup.RegisterGroup("ChampionMod:Tier2Broadswords", group);
 
-            group = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " World Evil Guns", new int[]
-            {
-                    ItemID.Musket,
-                    ItemID.TheUndertaker
-            });
-            RecipeGroup.RegisterGroup("ChampionMod:WorldEvilGuns", group);
-        }
+                        group = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " World Evil Guns", new int[]
+                        {
+                                ItemID.Musket,
+                        ItemID.TheUndertaker
+                        });
+                        RecipeGroup.RegisterGroup("ChampionMod:WorldEvilGuns", group);
+                }
 	}
 }
