2b9cf8317695e983c615254dc29f2da5bf4be04a
mariothedog <mariothedogmc@gmail.com>
2019-06-21 23:36:00 +0100

    Bunny wip

--- Projectiles/Minions/BunnyAI.cs
+++ Projectiles/Minions/BunnyAI.cs
@@ -73,7 +73,7 @@ namespace ChampionMod.Projectiles.Minions
             {
                 for (int k = 0; k < 200; k++)
                 {
-                    NPC npc = Main.npc[k];
+                    NPC npc = Main.npc[k]; // Goes through each npc
                     if (npc.CanBeChasedBy(this, false))
                     {
                         float distance = Vector2.Distance(npc.Center, projectile.Center);
@@ -99,11 +99,16 @@ namespace ChampionMod.Projectiles.Minions
                 // things like bullets and arrows don't need it most of the time but minions and pets do
                 // Be careful with netUpdate as setting it every tick can cause lag
             }
-            if (projectile.ai[0] == 1f)
+            // When projectile.ai[0] is 1 the minion is colliding with a tile
+            // When projectile.ai[1] is 0 the minion is not targetting anything
+            Main.NewText("AI 0: " + projectile.ai[0]);
+            Main.NewText("AI 1: " + projectile.ai[1]);
+
+            if (projectile.ai[0] == 1f) // If hitting tile
             {
-                projectile.tileCollide = true; // false
+                projectile.tileCollide = true;
             }
-            if (target && projectile.ai[0] == 0f)
+            if (target && projectile.ai[0] == 0f) // If targetting and not hitting tile
             {
                 Vector2 direction = targetPos - projectile.Center;
                 if (direction.Length() > chaseDist)
@@ -116,8 +121,10 @@ namespace ChampionMod.Projectiles.Minions
                     projectile.velocity *= (float)Math.Pow(0.97, 40.0 / inertia);
                 }
             }
-            else
+            else // If not targetting or hitting tile
             {
+                // Controls the direction and slowing down of the minion based on distance from player
+
                 if (!Collision.CanHitLine(projectile.Center, 1, 1, player.Center, 1, 1))
                 {
                     projectile.ai[0] = 1f;
@@ -168,6 +175,7 @@ namespace ChampionMod.Projectiles.Minions
                     projectile.velocity *= (float)Math.Pow(0.9, 40.0 / inertia);
                 }
             }
+            
             projectile.rotation = projectile.velocity.X * 0.05f;
             SelectFrame(Math.Abs(projectile.velocity.X) + Math.Abs(projectile.velocity.Y));
             //Main.NewText(Math.Abs(projectile.velocity.X) + Math.Abs(projectile.velocity.Y));
@@ -227,265 +235,10 @@ namespace ChampionMod.Projectiles.Minions
             }
         }
 
-        /*public override void Behavior()
-        {
-            Vector2 targetPos = projectile.position;
-            float targetDist = viewDist;
-            bool target = false;
-            //projectile.tileCollide = true;
-            for (int k = 0; k < 200; k++)
-            {
-                NPC npc = Main.npc[k]; // Main.npc is a list of npcs
-                if (npc.CanBeChasedBy(this, false))
-                {
-                    float distance = Vector2.Distance(npc.Center, projectile.Center); // Get distance between enemy and minion
-
-                    // "distance < targetDist" checks if the distance between the enemy and minion is less than the max viewing distance of the minion
-                    if ((distance < targetDist || !target) && Collision.CanHitLine(projectile.position, projectile.width, projectile.height, npc.position, npc.width, npc.height))
-                    {
-                        targetDist = distance;
-                        targetPos = npc.Center;
-                        target = true;
-                    }
-                }
-            }
-
-            Player player = Main.player[projectile.owner];
-            MyPlayer modPlayer = player.GetModPlayer<MyPlayer>();
-            Vector2 playerRel = player.position - projectile.position;
-            float playerDist = Math.Abs(playerRel.X) + Math.Abs(playerRel.Y);
-            Vector2 playerDir = playerRel / new Vector2(Math.Abs(playerRel.X) + Math.Abs(playerRel.Y), Math.Abs(playerRel.X) + Math.Abs(playerRel.Y));
-            float totalVel = Math.Abs(projectile.velocity.X) + Math.Abs(projectile.velocity.Y);
-
-            projectile.spriteDirection = projectile.direction = (projectile.velocity.X < 0f).ToDirectionInt(); // Change direction of sprite
-
-            if (player.dead || !player.HasBuff(mod.BuffType("Bunny"))) // Delete minion if player dies or removes buff
-            {
-                modPlayer.Bunny = false;
-            }
-
-            if (modPlayer.Bunny)
-            {
-                projectile.timeLeft = 2;
-            }
-
-            if (playerDist >= 128 + Main.rand.Next(8)) // If player distance is greater than x then follow player
-            {
-                projectile.velocity += playerDir / 5;
-            }
-
-            if (totalVel <= 2) // Speed up/slow down minion
-            {
-                projectile.velocity *= new Vector2(1.03f, 1.03f);
-            }
-            else
-            {
-                projectile.velocity *= new Vector2(0.97f, 0.97f);
-            }
-
-            Vector2 direction = targetPos - projectile.Center;
-            if (direction.Length() > chaseDist)
-            {
-                direction.Normalize();
-                projectile.velocity = (projectile.velocity * inertia + direction * chaseAccel) / (inertia + 1);
-            }
-            else
-            {
-                projectile.velocity *= (float)Math.Pow(0.97, 40.0 / inertia);
-            }
-
-            SelectFrame(totalVel);
-            
-            //Dust.NewDust(projectile.position, projectile.width, projectile.height, 180);
-        }*/
-
-
-        /*public override void Behavior()
-        {
-            Player player = Main.player[projectile.owner];
-            float spacing = (float)projectile.width * spacingMult;
-            for (int k = 0; k < 1000; k++)
-            {
-                Projectile otherProj = Main.projectile[k];
-                if (k != projectile.whoAmI && otherProj.active && otherProj.owner == projectile.owner && otherProj.type == projectile.type && System.Math.Abs(projectile.position.X - otherProj.position.X) + System.Math.Abs(projectile.position.Y - otherProj.position.Y) < spacing)
-                {
-                    if (projectile.position.X < Main.projectile[k].position.X)
-                    {
-                        projectile.velocity.X -= idleAccel;
-                    }
-                    else
-                    {
-                        projectile.velocity.X += idleAccel;
-                    }
-                    if (projectile.position.Y < Main.projectile[k].position.Y)
-                    {
-                        projectile.velocity.Y -= idleAccel;
-                    }
-                    else
-                    {
-                        projectile.velocity.Y += idleAccel;
-                    }
-                }
-            }
-            Vector2 targetPos = projectile.position;
-            float targetDist = viewDist;
-            bool target = false;
-            projectile.tileCollide = true;
-            // I think this is for tracking monsters
-            for (int k = 0; k < 200; k++)
-            {
-                NPC npc = Main.npc[k];
-                if (npc.CanBeChasedBy(this, false))
-                {
-                    float distance = Vector2.Distance(npc.Center, projectile.Center);
-                    if ((distance < targetDist || !target) && Collision.CanHitLine(projectile.position, projectile.width, projectile.height, npc.position, npc.width, npc.height))
-                    {
-                        targetDist = distance;
-                        targetPos = npc.Center;
-                        target = true;
-                    }
-                }
-            }
-            if (Vector2.Distance(player.Center, projectile.Center) > (target ? 1000f : 500f))
-            {
-                projectile.ai[0] = 1f;
-                projectile.netUpdate = true;
-            }
-            if (projectile.ai[0] == 1f)
-            {
-                projectile.tileCollide = false;
-            }
-            if (target && projectile.ai[0] == 0f)
-            {
-                Vector2 direction = targetPos - projectile.Center;
-                if (direction.Length() > chaseDist)
-                {
-                    direction.Normalize();
-                    projectile.velocity = (projectile.velocity * inertia + direction * chaseAccel) / (inertia + 1);
-                }
-                else
-                {
-                    projectile.velocity *= (float)Math.Pow(0.97, 40.0 / inertia);
-                }
-            }
-            else
-            {
-                if (!Collision.CanHitLine(projectile.Center, 1, 1, player.Center, 1, 1))
-                {
-                    projectile.ai[0] = 1f;
-                }
-                float speed = 6f;
-                if (projectile.ai[0] == 1f)
-                {
-                    speed = 15f;
-                }
-                Vector2 center = projectile.Center;
-                Vector2 direction = player.Center - center;
-                projectile.ai[1] = 3600f;
-                projectile.netUpdate = true;
-                int num = 1;
-                for (int k = 0; k < projectile.whoAmI; k++)
-                {
-                    if (Main.projectile[k].active && Main.projectile[k].owner == projectile.owner && Main.projectile[k].type == projectile.type)
-                    {
-                        num++;
-                    }
-                }
-                direction.X -= (float)((10 + num * 40) * player.direction);
-                direction.Y -= 70f;
-                float distanceTo = direction.Length();
-                if (distanceTo > 200f && speed < 9f)
-                {
-                    speed = 9f;
-                }
-                if (distanceTo < 100f && projectile.ai[0] == 1f && !Collision.SolidCollision(projectile.position, projectile.width, projectile.height))
-                {
-                    projectile.ai[0] = 0f;
-                    projectile.netUpdate = true;
-                }
-                if (distanceTo > 2000f)
-                {
-                    projectile.Center = player.Center;
-                }
-                if (distanceTo > 48f)
-                {
-                    direction.Normalize();
-                    direction *= speed;
-                    float temp = inertia / 2f;
-                    projectile.velocity = (projectile.velocity * temp + direction) / (temp + 1);
-                }
-                else
-                {
-                    projectile.direction = Main.player[projectile.owner].direction;
-                    projectile.velocity *= (float)Math.Pow(0.9, 40.0 / inertia);
-                }
-            }
-            projectile.rotation = projectile.velocity.X * 0.05f;
-            SelectFrame();
-            CreateDust();
-            if (projectile.velocity.X > 0f)
-            {
-                projectile.spriteDirection = (projectile.direction = -1);
-            }
-            else if (projectile.velocity.X < 0f)
-            {
-                projectile.spriteDirection = (projectile.direction = 1);
-            }
-            if (projectile.ai[1] > 0f)
-            {
-                projectile.ai[1] += 1f;
-                if (Main.rand.Next(3) == 0)
-                {
-                    projectile.ai[1] += 1f;
-                }
-            }
-            if (projectile.ai[1] > shootCool)
-            {
-                projectile.ai[1] = 0f;
-                projectile.netUpdate = true;
-            }
-            if (projectile.ai[0] == 0f)
-            {
-                if (target)
-                {
-                    if ((targetPos - projectile.Center).X > 0f)
-                    {
-                        projectile.spriteDirection = (projectile.direction = -1);
-                    }
-                    else if ((targetPos - projectile.Center).X < 0f)
-                    {
-                        projectile.spriteDirection = (projectile.direction = 1);
-                    }
-                    if (projectile.ai[1] == 0f)
-                    {
-                        projectile.ai[1] = 1f;
-                        if (Main.myPlayer == projectile.owner)
-                        {
-                            Vector2 shootVel = targetPos - projectile.Center;
-                            if (shootVel == Vector2.Zero)
-                            {
-                                shootVel = new Vector2(0f, 1f);
-                            }
-                            shootVel.Normalize();
-                            shootVel *= shootSpeed;
-                            int proj = Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, shootVel.X, shootVel.Y, shoot, projectile.damage, projectile.knockBack, Main.myPlayer, 0f, 0f);
-                            Main.projectile[proj].timeLeft = 300;
-                            Main.projectile[proj].netUpdate = true;
-                            projectile.netUpdate = true;
-                        }
-                    }
-                }
-            }
-        }*/
-
-
-
         public override bool TileCollideStyle(ref int width, ref int height, ref bool fallThrough)
         {
-            fallThrough = false;
+            fallThrough = false; // Can fall through platforms
             return true;
         }
-
-        
     }
 }
