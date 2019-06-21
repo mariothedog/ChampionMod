b0bed36b1084cd0e02ed93a8e57164604835dd89
Cornetto315 <51300570+Cornetto315@users.noreply.github.com>
2019-06-21 15:37:36 -0700

    Add files via upload

---
+++ Items/Weapons/FrozenChakram.cs
@@ -0,0 +1,53 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+ 
+namespace Champion.Items.Weapons
+{
+    public class FrozenChakram : ModItem
+    {
+        public override void SetStaticDefaults() {
+			 DisplayName.SetDefault("Frozen Chakram");
+		}
+		
+		public override void SetDefaults() {
+            item.damage = 19;            
+            item.melee = true;
+            item.width = 40;
+            item.height = 40;
+            item.useTime = 15;
+            item.useAnimation = 25;
+            item.noUseGraphic = true;
+            item.useStyle = 1;
+            item.knockBack = 8;
+            item.value = Item.sellPrice(silver : 2);
+            item.rare = 1;
+            item.shootSpeed = 11f;
+            item.shoot = mod.ProjectileType ("FrozenChakramProjectile");
+            item.UseSound = SoundID.Item1;
+            item.autoReuse = true;
+        }
+        public override bool CanUseItem(Player player)     
+        {
+            for (int i = 0; i < 1000; ++i)
+            {
+                if (Main.projectile[i].active && Main.projectile[i].owner == Main.myPlayer && Main.projectile[i].type == item.shoot)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public override void AddRecipes(){
+                ModRecipe recipe = new ModRecipe(mod);
+                recipe.AddIngredient(ItemID.BorealWood, 20);
+				recipe.AddIngredient(ItemID.Silk, 5);
+				recipe.AddIngredient(ItemID.SilverBar, 6);
+				recipe.AddIngredient(ItemID.IceTorch, 1);
+				recipe.AddTile(TileID.Anvils); 
+                recipe.SetResult(this);
+                recipe.AddRecipe();
+        }
+    }
+}
