68ff7ce0d98669640c0a834ad3ce72fe33211789
mariothedog <mariothedogmc@gmail.com>
2019-07-09 00:01:28 +0100

    naturesprotection wip

---
+++ Items/Accessories/NaturesProtection.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ChampionMod.Items.Accessories
+{
+    class NaturesProtection : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Nature's Protection");
+            Tooltip.SetDefault("Increases your defense by 5 and regenerates the health of the players around you by 2 every second" +
+                "\nYour regen is heavily nerfed");
+        }
+
+        public override void SetDefaults()
+        {
+            item.accessory = true;
+            item.defense = 5;
+            item.width = 40;
+            item.height = 40;
+            item.noUseGraphic = true; // So you don't see the item swing (and you just see the projectile)
+            item.knockBack = 8;
+            item.value = Item.sellPrice(silver : 2);
+            item.rare = 1;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.BorealWood, 20);
+            recipe.AddIngredient(ItemID.Silk, 5);
+            recipe.AddRecipeGroup("ChampionMod:Tier3Bars", 6);
+            recipe.AddIngredient(ItemID.IceTorch, 1);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            //player.allDamage += 19f; // increase all damage by 1900%
+                                        /* Here are the individual weapon class bonuses.
+                                        player.meleeDamage += 19f;
+                                        player.thrownDamage += 19f;
+                                        player.rangedDamage += 19f;
+                                        player.magicDamage += 19f;
+                                        player.minionDamage += 19f;
+                                        */
+            //player.endurance = 1f - 0.1f * (1f - player.endurance);
+            player.GetModPlayer<MyPlayer>().NaturesProtection = true;
+            //Main.NewText(player.lifeRegen);
+            //Main.NewText(item.lifeRegen);
+            //player.lifeRegen = -100;
+
+            //player.statDefense = 0;
+            //player.allDamage = 0.1f;
+            //player.lifeRegen = -120;
+        }
+    }
+}
--- MyPlayer.cs
+++ MyPlayer.cs
@@ -12,18 +12,19 @@ namespace ChampionMod
 {
     public class MyPlayer : ModPlayer
     {
-        private const int saveVersion = 0;
         public bool Bunny = false;
         public static bool hasProjectile;
         public bool BunnyMinion;
         public float LastDeathX;
         public float LastDeathY;
         public int memoryTimer = -1;
+        public bool NaturesProtection = false;
 
         public override void ResetEffects()
         {
             Bunny = false;
             BunnyMinion = false;
+            NaturesProtection = false;
         }
 
         public override void CatchFish(Item fishingRod, Item bait, int power, int liquidType, int poolSize, int worldLayer, int questFish, ref int caughtType, ref bool junk)
@@ -82,6 +83,52 @@ namespace ChampionMod
                 {
                     Dust.NewDust(new Vector2(player.position.X-40, player.position.Y), 110, 110, 27, 0f, 0f, 100, default(Color), 1.6f);
                 }
+            }
+        }
+
+        public override void UpdateLifeRegen()
+        {
+            if (NaturesProtection)
+            {
+                //player.lifeRegen /= 10;
+
+                //player.lifeRegenTime = (int)(player.lifeRegenTime / 1.2);
+                //player.lifeRegenCount -= 1;
+                //player.lifeRegenTime -= 1;
+                //player.lifeRegenTime /= 2;
+                //player.lifeRegen -= 1;
+                //player.lifeRegenCount -= 1;
+                //Main.NewText(player.lifeRegen);
+                //Main.NewText(player.lifeRegenTime);
+                //Main.NewText(player.lifeRegenCount);
+
+                /*float num2 = 0f;
+                num2 = ((!player.shinyStone) ? (num2 / 2f) : (num2 * 0.75f));
+                PlayerHooks.NaturalLifeRegen(player, ref num2);
+                float num4 = (float)player.statLifeMax2 / 400f * 0.85f + 0.15f;
+                num2 *= num4;
+                player.lifeRegen += (int)Math.Round((double)num2);
+                player.lifeRegenCount += player.lifeRegen;*/
+
+                /*player.lifeRegen -= 1;
+
+                if (player.lifeRegen < 0)
+                {
+                    player.lifeRegen = 0;
+                }*/
+
+                /*if (player.lifeRegenTime >= 300)
+                {
+                    player.lifeRegen += 1;
+                    player.lifeRegenCount += player.lifeRegen;
+                }*/
+
+                if (player.lifeRegenTime >= 300)
+                {
+                    player.lifeRegenTime = 300;
+                }
+
+                Main.NewText(player.lifeRegenTime);
             }
         }
     }
