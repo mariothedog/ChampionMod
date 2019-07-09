cb1a3165add351c01d9da832937dc93b1227f565
mariothedog <mariothedogmc@gmail.com>
2019-07-10 00:10:24 +0100

    royal sword done for update but needs dust

--- Items/Weapons/Swords/RoyalSword.cs
+++ Items/Weapons/Swords/RoyalSword.cs
@@ -39,5 +39,36 @@ namespace ChampionMod.Items.Weapons.Swords
         {
             target.AddBuff(BuffID.Slimed, 1200, false); // Slimed debuff for 20 seconds
         }
+
+        /*public override void MeleeEffects(Player player, Rectangle hitbox)
+        {
+            if (Main.rand.NextFloat() < 1f)
+            {
+                Dust dust;
+                // You need to set position depending on what you are doing. You may need to subtract width/2 and height/2 as well to center the spawn rectangle.
+                //Vector2 position = Main.LocalPlayer.Center;
+                dust = Main.dust[Terraria.Dust.NewDust(item.position, 30, 30, 13, 0f, 0f, 0, new Color(255, 255, 255), 2.236842f)];
+            }
+
+
+            /*if (Main.rand.Next(5) == 0) // Lowers amount of dust
+            {
+                int dustChoice;
+                switch (Main.rand.Next(3))
+                {
+                    case 0:
+                        dustChoice = 15;
+                        break;
+                    case 1:
+                        dustChoice = 57;
+                        break;
+                    default:
+                        dustChoice = 58;
+                        break;
+                }
+                Dust enchantedDust = Main.dust[Dust.NewDust(new Vector2((float)hitbox.X, (float)hitbox.Y), hitbox.Width, hitbox.Height, dustChoice, (float)(player.direction * 2), 0f, 150, default(Color), 1.3f)];
+                enchantedDust.velocity *= 0.2f;
+            }*/
+        }*/
     }
 }
