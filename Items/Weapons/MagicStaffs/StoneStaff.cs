044ba5ada2bbb87fd82e0a5006fa104dbb6da1a5
Cornetto315 <51300570+Cornetto315@users.noreply.github.com>
2019-07-18 13:28:10 -0700

    Update StoneStaff.cs

--- Items/Weapons/MagicStaffs/StoneStaff.cs
+++ Items/Weapons/MagicStaffs/StoneStaff.cs
@@ -1,17 +1,17 @@
-using Microsoft.Xna.Framework
+using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace ChampionMod.Items.Weapons.Magicstaffs
+namespace Champion.Items.Weapons
 {
 	public class StoneStaff : ModItem
 	{
-		public override void SetStaticdefaults() {
-      DisplayName.SetDefaults("Stone Staff")
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Stone Staff");
 			Item.staff[item.type] = true;
 		}
-
+		
 		public override void SetDefaults() {
 			item.damage = 8;
 			item.magic = true;
@@ -19,23 +19,23 @@ namespace ChampionMod.Items.Weapons.Magicstaffs
 			item.width = 48;
 			item.height = 58;
 			item.useTime = 31;
-		  item.useAnimation = 31;
+			item.useAnimation = 31;
 			item.useStyle = 5;
-			item.noMelee = true; //so the item's animation doesn't do damage
+			item.noMelee = true; 
 			item.knockBack = 6;
 			item.value = 1;
 			item.rare = 1;
 			item.UseSound = SoundID.Item20;
 			item.autoReuse = true;
 			item.shoot = mod.ProjectileType("StoneBoulder");
-			item.shootSpeed = 3f;
-		}
+			item.shootSpeed = 8f;
+	    }
 
 		public override void AddRecipes() {
 			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ItemID.Stone, 20);
-      			recipe.AddIngredient(ItemID.Wood, 15);
-     		 	recipe.AddIngredient(ItemID.IronBar, 2);
+			recipe.AddIngredient(ItemID.StoneBlock, 20);
+      		recipe.AddIngredient(ItemID.Wood, 15);
+     		recipe.AddIngredient(ItemID.IronBar, 2);
 			recipe.AddTile(TileID.Anvils);
 			recipe.SetResult(this);
 			recipe.AddRecipe();
