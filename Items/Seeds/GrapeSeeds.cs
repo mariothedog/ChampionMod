d7457f55ddf2627f4749a6a1dfc309e2bb558561
mariothedog <mariothedogmc@gmail.com>
2019-08-27 18:17:57 +0100

    Grape stuff (I forgot to commit)

---
+++ Items/Seeds/GrapeSeeds.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ChampionMod.Items.Seeds
+{
+    class GrapeSeeds : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 22;
+            item.maxStack = 99;
+            item.autoReuse = true;
+            item.useTime = 10;
+            item.useAnimation = 15;
+            item.consumable = true;
+            item.createTile = mod.TileType("GrapeCrop");
+            item.useStyle = 1;
+            item.placeStyle = 0;
+            item.value = 80; // 16 copper like the vanilla seeds
+            item.useTurn = true;
+        }
+    }
+}
