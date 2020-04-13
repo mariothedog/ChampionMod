6144b21af558ddf5dd26cb10e4f8b3f88edf2bf3
Stevie <xxlennygamerxx@gmail.com>
2020-04-13 13:20:56 -0700

    Reinforced Wooden Helmet (Needs sprite)

---
+++ Items/Armor/ReinforcedWoodenHelmet.cs
@@ -0,0 +1,28 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace ChampionMod.Items.Armor
+{
+    [AutoloadEquip(EquipType.Head)]
+    class ReinforcedWoodenHelmet : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.CloneDefaults(ItemID.WoodHelmet);
+            item.defense = 2;
+        }
+        public override bool IsArmorSet(Item head, Item body, Item legs)
+        {
+            return body.type == ItemType<ReinforcedWoodenBreastplate>() && legs.type == ItemType<ReinforcedWoodenGreaves>();
+        }
+        public override void UpdateArmorSet(Player player)
+        {
+            player.setBonus = "+1 defense" +
+                "\nIncreases armor penetration by 5";
+            player.armorPenetration += 5;
+            //player.statDefense += 1; //Not sure why but it automatically increases defense by 1 without this for me. If it doesn't for you, add this back in, might just be a small issue on my side.
+        }
+    }
+}
