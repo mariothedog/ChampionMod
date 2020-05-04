6aa6ec4612478af22417d157f9e47592a6b2ca74
mariothedog <mariothedogmc@gmail.com>
2020-05-04 19:07:01 +0100

    Nerfed the immolator.

--- Items/Weapons/Melee/Swords/Immolator.cs
+++ Items/Weapons/Melee/Swords/Immolator.cs
@@ -1,34 +1,33 @@
 using Microsoft.Xna.Framework;
-using System;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace ChampionMod.Items.Weapons.Melee.Swords
 {
-	public class Immolator : ModItem
+    public class Immolator : ModItem
 	{
 		public override void SetDefaults()
         {
-			item.damage = 70;           
-			item.melee = true;          
-			item.width = 44;           
-			item.height = 44;           
-			item.useTime = 34;          
-			item.useAnimation = 34;         
-			item.useStyle = 1;          
-			item.knockBack = 8;         
-			item.value = Item.buyPrice(gold: 1);           
-			item.rare = 3;              
-			item.UseSound = SoundID.Item1;      
-			item.autoReuse = true;          
+			item.damage = 60;
+			item.melee = true;
+			item.width = 44;
+			item.height = 44;
+			item.useTime = 38;
+			item.useAnimation = 38;
+			item.useStyle = 1;
+			item.knockBack = 8;
+			item.value = Item.buyPrice(gold: 1);
+			item.rare = 3;
+			item.UseSound = SoundID.Item1;
 		}
 
 		public override void MeleeEffects(Player player, Rectangle hitbox)
         {
-			if (Main.rand.NextBool(3)) {
+			if (Main.rand.NextBool(3))
+            {
 				// Emit dust when swinging the sword
-				Dust.NewDust(new Vector2((float)hitbox.X, (float)hitbox.Y), hitbox.Width, hitbox.Height, 127, 0f, 0f, 255, default);
+				Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, 127, 0f, 0f, 255, default);
 			}
 		}
 
@@ -55,7 +54,7 @@ namespace ChampionMod.Items.Weapons.Melee.Swords
             }
 
             // Large Smoke Gore spawn
-            int goreIndex = Gore.NewGore(new Vector2(target.position.X + (float)(target.width / 2) - 24f, target.position.Y + (float)(target.height / 2) - 24f), default, Main.rand.Next(61, 64), 0.8f);
+            int goreIndex = Gore.NewGore(new Vector2(target.position.X + target.width / 2 - 24f, target.position.Y + target.height / 2 - 24f), default, Main.rand.Next(61, 64), 0.8f);
             Main.gore[goreIndex].scale = 1.05f;
             Main.gore[goreIndex].velocity.Y = Main.gore[goreIndex].velocity.Y + 1.2f;
         }
